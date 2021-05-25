33e23eddf045095fa90af11e6cc337d606a202f6
Mandell Butler <mandell.butler@outlook.com>
2021-05-25 15:51:26 -0400

    Added login route and templates

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PW,
+    process.env.DB_PASSWORD,
     {
       host: process.env.DB_HOST || 'localhost',
       dialect: 'mysql',
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
-const router = require('express').Router()
-const apiRoutes = require('./api')
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require("./home-routes");
 
 // api routes
 router.use('/api', apiRoutes)
 
 // index route
-router.use('/', (req, res) => {
-  res.render('homepage')
-})
+router.use('/', homeRoutes);
 
 module.exports = router
