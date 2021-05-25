4af1eb8bad0c1dd66413e1d3a6f9473ffd14e42b
Jason Lam <nyjasonlam@gmail.com>
2021-05-25 13:46:38 -0400

    Added jaws db env var to db connection

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,17 @@
 const Sequelize = require('sequelize')
 require('dotenv').config()
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PW,
-  {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql',
-    port: process.env.DB_PORT || 3306
-  }
-)
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: process.env.DB_HOST || 'localhost',
+      dialect: 'mysql',
+      port: process.env.DB_PORT || 3306
+    }
+  )
 
 module.exports = sequelize
