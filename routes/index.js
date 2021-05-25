85f4ad84950328394d58957e5ee807d5ab6352c6
Jason Lam <nyjasonlam@gmail.com>
2021-05-25 13:17:53 -0400

    Added a default api response route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 const router = require('express').Router()
+const apiRoutes = require('./api')
 
+// api routes
+router.use('/api', apiRoutes)
+
+// index route
 router.use('/', (req, res) => {
   res.json('hello world')
 })
