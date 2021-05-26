358c032d2cb9d09badf041f59077977f476568be
Mandell Butler <mandell.butler@outlook.com>
2021-05-26 12:19:42 -0400

    Completed seeds for user and movie

--- models/Rating.js
+++ models/Rating.js
@@ -3,24 +3,28 @@ const sequelize = require('../config/connection')
 
 class Rating extends Model {}
 
-Rating.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-    foreignKey: 'movie_id',
-    foreignKey: 'user_id'
+Rating.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      foreignKey: 'movie_id',
+      foreignKey: 'user_id'
+    },
+    user_rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   },
-  user_rating: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  sequelize,
-  timestamps: true,
-  freezeTablename: false,
-  underscored: true,
-  modelName: 'Rating'
-})
+  {
+    sequelize,
+    timestamps: true,
+    freezeTablename: false,
+    underscored: true,
+    modelName: 'Rating'
+  }
+)
 
 module.exports = Rating
