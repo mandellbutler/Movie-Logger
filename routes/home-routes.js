a6708e152b94f7efad13634f1f1b2a28adb936d7
Jason Lam <nyjasonlam@gmail.com>
2021-05-26 10:17:47 -0400

    Added log out route

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -9,7 +9,7 @@ router.get('/login', (req, res) => {
 })
 
 router.get('/', (req, res) => {
-  res.render('homepage', { loggedIn: req.sessions.loggedIn })
+  res.render('homepage', { loggedIn: req.session.loggedIn })
 })
 
 module.exports = router
