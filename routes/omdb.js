5bef78254ffb506e1dfb284d19b755b8cbddba26
Jason Lam <nyjasonlam@gmail.com>
2021-05-26 12:40:46 -0400

    added search by imdbID route

--- routes/omdb.js
+++ routes/omdb.js
@@ -4,19 +4,40 @@ require('dotenv').config();
 
 const apiKey = process.env.OMDB_APIKEY;
 
-router.get('/search', async (req, res) => {
-  // const search = req.body.search;
+// search by movie imdbID
+router.get('/search/imdb', async (req, res) => {
   const imdbID = 'tt0126029';
-  // const baseSearchUrl = `http://www.omdbapi.com/?apikey=${apiKey}&s=${search}`;
-  const testUrl = `http://www.omdbapi.com/?apikey=${apiKey}&i=${imdbID}`;
+  const baseSearchByIdUrl = `http://www.omdbapi.com/?apikey=${apiKey}&i=${imdbID}`;
   try {
-    const movieData = await axios.get(testUrl);
+    const movieData = await axios.get(baseSearchByIdUrl);
     const data = await movieData.data;
     res.json(data);
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+// search by title
+router.get('/search/title', async (req, res) => {
+  const search = req.body.search;
+  const baseSearchByTitleUrl = `http://www.omdbapi.com/?apikey=${apiKey}&s=${search}`;
+  try {
+    const movieData = await axios.get(baseSearchByTitleUrl);
+    const data = await movieData.data;
+    res.json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// search by title
+// get title's imdb id
+// check if movie is already in database by ID as movie pk
+// if it is,
+// return the values
+// if not,
+// fetch from web api
+// put it into our own database
 
 module.exports = router;
 
