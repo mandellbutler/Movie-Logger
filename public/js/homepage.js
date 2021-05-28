247a7c848fb208e49b0cba31152bd75b90657f94
Mandell Butler <mandell.butler@outlook.com>
2021-05-28 08:01:00 -0400

    Activated image swiper with live images and automation

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,24 +1,30 @@
 window.addEventListener ('DOMContentLoaded', () => {
   console.log('Hello World');
 });
-const swiper = new Swiper('.swiper-container', {
-  speed: 400,
-  spaceBetween: 100,
-  pagination: {
-    el: '.swiper-pagination',
-  },
 
-  // Navigation arrows
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
 });
-console.log(swiper);
-// Now you can use all slider methods like
-// swiper.slideNext();
+
