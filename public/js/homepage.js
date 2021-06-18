ec597e1249bce52dda86be4b2f053b8e84b6a80d
Mandell Butler <mandell.butler@outlook.com>
2021-06-18 08:40:14 -0400

    prep for merge

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -2,6 +2,7 @@ window.addEventListener ('DOMContentLoaded', () => {
   console.log('Hello World');
 });
 
+// eslint-disable-next-line no-unused-vars
 var swiper = new Swiper('.mySwiper', {
   effect: 'coverflow',
   grabCursor: true,
